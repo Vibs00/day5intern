b3460f84247c5dd79bb44bf2680d928d96fb5b98
vibs00 <vibha.ajm13@gmail.com>
2019-05-14 12:14:06 +0530

    task 2 complete

--- app.js
+++ app.js
@@ -11,3 +11,11 @@ function c_f(c){
 function f_c(f){
     return (((f-32)*5)/9) + "°C";
 }
+
+function max(a,b,c){
+    return (a>b)?(a>c?a:c) : (b>c?b:c);
+}
+
+function factor(num){
+    return (num%3==0)?true : (num%7==0?true:false);
+}
